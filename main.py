d847d1f21fed5fedb99ff9df27eac711aed09387
amrithaasesh <47097017+amrithaasesh@users.noreply.github.com>
2019-01-28 22:09:18 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,53 @@
+'''
+WELCOME TO MY MUSIC STUDIO
+HERE ARE THE DETAILS OF MY 'TOP 3 CHARTBUSTERS'
+GET READY TO GROOVE
+'''
+
+#SONG - 1
+name="All of Me"
+artist="John Legend"
+album="Love in the Future"
+time="269" + "secs" #Run-time of the song
+Release="August 12, 2013"
+Funfact="It was shot in Italy, just days before Legend's wedding. It's his love song"
+print('##########################################\n')
+print('SONG-1:',name, "by", artist,"\n")
+print('##########################################\n')
+print("Album:", album, "\n")
+print("Length of the Song: ", time, "\n")
+print("Release Date: ", Release,'\n')
+print("DID YOU KNOW?", Funfact, "\n")
+
+#SONG - 2
+name="Fix You"
+artist="Coldplay"
+album="X&Y"
+time="294" + "secs" #Run-time of the song
+Release="September 5, 2005"
+Funfact="Fix You is influenced by English alternative rock band Elbow's 2003 anthem Grace Under Pressure"
+print('##########################################\n')
+print('SONG-2:',name, "by", artist,"\n")
+print('##########################################\n')
+print("Album:", album, "\n")
+print("Length of the Song: ", time, "\n")
+print("Release Date: ", Release,'\n')
+print("DID YOU KNOW?", Funfact, "\n")
+
+#SONG - 3
+name="Thinking out Loud" #Name of the song
+artist="Ed Sheeran"
+album="x" 
+time="281" + "secs" #Run-time of the song
+Release="September 24, 2014" #Release Date
+Funfact="The song was composed last minute, when Sheeran and Wadge caught on to a tune while jamming casually"
+print('##########################################\n')
+print('SONG-3:',name, "by", artist,"\n")
+print('##########################################\n')
+print("Album:", album, "\n")
+print("Length of the Song: ", time, "\n")
+print("Release Date: ", Release,'\n')
+print("DID YOU KNOW?", Funfact, "\n")
+
+print('\n##########################################\n')
+print("I hope you stream to my favorites now!")
